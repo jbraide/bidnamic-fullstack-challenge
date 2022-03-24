a8c969b35df3e9cf5e59e28b5cfdbd9265c3d6f6
Joseph Braide <braidej@gmail.com>
2022-03-24 21:35:18 +0100

    validation for invalid bid setting option

--- bidnamic_interview/multistepform/forms.py
+++ bidnamic_interview/multistepform/forms.py
@@ -29,5 +29,18 @@ class BiddingInformationForm(forms.ModelForm):
     class Meta:
         model = BioDataAndBiddingInformation
         fields = ['bidding_setting', 'google_ads_id']
+    
+    def clean_bidding_setting(self):
+        # get the tuple of bidding_settings for BioData.... Model
+        bidding_settings = BioDataAndBiddingInformation.bidding_settings
 
+        # collect the bidding setting option that was selected or inputted
+        bidding_setting_option = self.cleaned_data['bidding_setting']
+
+        # loop through the option
+        for bidding_option in bidding_settings:
+            if bidding_setting_option != bidding_option[0]:
+                raise forms.ValidationError('This option is an Invalid Bidding Setting')
+            else:
+                return bidding_setting_option
 
