b2ab95505397409ce36e66db740443ac9ded7177
Joseph Braide <braidej@gmail.com>
2022-03-24 14:09:17 +0100

    installed phonenumbers dependency and moved all package updates to requirements.txt in project level folder

--- bidnamic_interview/multistepform/models.py
+++ bidnamic_interview/multistepform/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-class 
+# import phonenumber_field
+from phonenumber_field.modelfields import PhoneNumberField
+
+
+class BioDataAndBiddingInformation(models.Model):
+    title = models.CharField(max_length=50)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    date_of_birth = models.DateField()
+    company_name = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
+    phone_number = PhoneNumberField()
+    
