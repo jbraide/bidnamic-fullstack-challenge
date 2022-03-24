a944930afffdc61f7c4fccbc8a9261d592bf668c
Joseph Braide <braidej@gmail.com>
2022-03-24 21:58:32 +0100

    initial tests for valid and invalid bidding form options

--- bidnamic_interview/multistepform/tests.py
+++ bidnamic_interview/multistepform/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .forms import BiddingInformationForm, BioDataForm
+
+class TestBiddingInformationForm(TestCase):
+    def test_invalid_bidding_option_in_form(self):
+        invalid_bidding_option = {
+            'bidding_setting':'Invalid Option',
+            'google_ads_id':'123-902-83727'
+        }
+        bidding_form = BiddingInformationForm(invalid_bidding_option).is_valid()
+        return self.assertFalse(bidding_form, 'Invalid Option selected')
+
+    def test_valid_bidding_form(self):
+        valid_bidding_data = {
+            'bidding_setting':'High',
+            'google_ads_id':'123-902-83727'
+        }
+        bidding_form = BiddingInformationForm(valid_bidding_data).is_valid()
+        return self.assertTrue(bidding_form)
