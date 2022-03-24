f9e4b8a55f56431c6dccb6f78a42eb4af6738f39
Joseph Braide <braidej@gmail.com>
2022-03-24 14:13:43 +0100

    included bidding setting and google ads id to model and migrated

--- bidnamic_interview/multistepform/models.py
+++ bidnamic_interview/multistepform/models.py
@@ -5,6 +5,11 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 
 class BioDataAndBiddingInformation(models.Model):
+    bidding_settings = (
+        ('High', 'High'), 
+        ('Medium', 'Medium'), 
+        ('Low', 'Low'), 
+    )
     title = models.CharField(max_length=50)
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
@@ -12,4 +17,5 @@ class BioDataAndBiddingInformation(models.Model):
     company_name = models.CharField(max_length=200)
     address = models.CharField(max_length=200)
     phone_number = PhoneNumberField()
-    
+    bidding_setting = models.CharField(max_length=20, choices=bidding_settings)
+    google_ads_id = models.CharField(max_length=40)
