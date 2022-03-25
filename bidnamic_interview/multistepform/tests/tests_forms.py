02d3e9c0bb086f1c2b7ae89641d5c5c0d7a570e4
Joseph Braide <braidej@gmail.com>
2022-03-25 08:04:04 +0100

    changes to some form testing structure and wrote test on empty bio_data_form

--- bidnamic_interview/multistepform/tests/tests_forms.py
+++ bidnamic_interview/multistepform/tests/tests_forms.py
@@ -3,6 +3,8 @@ from django.test import TestCase
 # get the BiddingInformationForm and BioDataForm from multistep
 from multistepform.forms import BiddingInformationForm, BioDataForm
 
+# import date from datetime module
+from datetime import date 
 
 class TestBiddingInformationForm(TestCase):
     def test_invalid_bidding_option_in_form(self):
@@ -10,8 +12,16 @@ class TestBiddingInformationForm(TestCase):
             'bidding_setting':'Invalid Option',
             'google_ads_id':'123-902-83727'
         }
-        bidding_form = BiddingInformationForm(invalid_bidding_option).is_valid()
-        return self.assertFalse(bidding_form, 'Invalid Option selected')
+
+        # Create the biddinginfoform instance and check the status by .is_valid 
+        bidding_form = BiddingInformationForm(invalid_bidding_option)
+        validate_bidding_form = bidding_form.is_valid()
+
+        
+        # collect the error message and pass is as a parameter 
+        error_message = bidding_form.errors
+
+        return self.assertFalse(validate_bidding_form, error_message)
 
     def test_valid_bidding_form(self):
         valid_bidding_data = {
@@ -23,6 +33,7 @@ class TestBiddingInformationForm(TestCase):
 
 
 class TestBioDataForm(TestCase):
+
     # validate date entered is under 18 yrs of age.
     def test_dob_under_18_requirement(self):
         # get today and birthday
@@ -41,5 +52,11 @@ class TestBioDataForm(TestCase):
         }
         
         form = BioDataForm(form_data)
-        valid_form = form.is_valid()
-        return self.assertTrue(valid_form, form.errors)
+        validate_form = form.is_valid()
+        return self.assertFalse(validate_form, form.errors)
+
+    # Empty form data
+    def test_empty_bio_data_form_field(self):
+        form = BioDataForm()
+        validate_form = form.is_valid() 
+        return self.assertFalse(validate_form)
