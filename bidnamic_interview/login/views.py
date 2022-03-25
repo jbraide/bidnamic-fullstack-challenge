6cbfbb8b581d23a8f06d068b666b13a8d387f75b
Joseph Braide <braidej@gmail.com>
2022-03-25 11:10:28 +0100

    View to handle Login by inheriting from LoginView

--- bidnamic_interview/login/views.py
+++ bidnamic_interview/login/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+# import LoginView
+from django.contrib.auth.views import LoginView
+
+# import reverse_lazy
+from django.urls import reverse_lazy
+
+class Login(LoginView):
+    template_name = 'login/signin.html'
+    redirect_authenticated_user = True
